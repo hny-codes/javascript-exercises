4c062d2436eb3df4e64893b642196aabbb3fbca6
Henry <hnguyhenry@protonmail.com>
2022-05-25 21:57:08 -0700

    Update reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  if (string === "") {
+    return "";
+  }
+  const stringCopy = [...string];
+  
+  return stringCopy.reduce((sum, element) => {
+    return element + sum;
+  });
 };
 
 // Do not edit below this line
