87bac1bc695d862bc01cf9bfb5f307dd737a44b1
Henry <hnguyhenry@protonmail.com>
2022-05-25 21:24:10 -0700

    Update repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (input, max) {
+  let output = "";
+  if (max < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < max; i++) {
+    output += input;
+  }
 
+  return output;
 };
 
 // Do not edit below this line
