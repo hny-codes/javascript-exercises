cd6393f0b83e1f24063a98e8d7af15d8106b3023
Henry <hnguyhenry@protonmail.com>
2022-05-25 23:01:45 -0700

    Update removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+  const args = [...arguments];
+  const array = args[0];
+  const value = [...args];
+  value.shift();
+  console.log(array);
+  console.log(value);
+  return array.filter((element) => {
+    return !value.find((num) => num == element);
+  });
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+removeFromArray([1, 2, 3, 4], 3);
