211d56bb4f353b95e6a541ff10ab848f36778671
Henry <hnguyhenry@protonmail.com>
2022-05-26 19:59:54 -0700

    Update Fahrenheit and Celsius functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  const newTemp = ((temp - 32) * 5) / 9;
+  return parseFloat(newTemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  const newTemp = temp * 1.8 + 32;
+  return parseFloat(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
