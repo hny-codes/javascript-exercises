cd76886ab7a02d4d7b99642b8d2556789ca99abd
Henry <hnguyhenry@protonmail.com>
2022-05-26 19:05:14 -0700

    Update sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  if (
+    Number.isInteger(first) == false ||
+    Number.isInteger(second) == false ||
+    first <= 0 ||
+    second <= 0
+  ) {
+    return "ERROR";
+  }
 
+  const range = [first, second].sort((a, b) => a - b);
+  let firstNumber = range[0];
+  const secondNumber = range[1];
+  let sum = 0;
+
+  console.log(range);
+  for (firstNumber; firstNumber <= secondNumber; firstNumber++) {
+    sum += firstNumber;
+  }
+  return sum;
 };
 
 // Do not edit below this line
